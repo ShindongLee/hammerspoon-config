4abfbf9320e1113ce411d6a869fa939e314c2501
Shindong <koko9411@gmail.com>
2022-03-24 14:57:17 +0900

    add init.lua

---
+++ init.lua
@@ -0,0 +1,22 @@
+local input_source = hs.keycodes.currentSourceID()
+local inputEnglish = "com.apple.keylayout.ABC"
+local esc_bind
+
+if not (input_source == inputEnglish) then
+    hs.eventtap.keyStroke({}, 'right')
+    hs.keycodes.currentSourceID(inputEnglish)
+    hs.eventtap.keyStroke({}, 'escape')
+end
+hs.eventtap.keyStroke({}, 'escape')
+
+function back_to_eng()
+	local inputSource = hs.keycodes.currentSourceID()
+	if not (inputSource == inputEnglish) then
+		hs.keycodes.currentSourceID(inputEnglish)
+	end
+	esc_bind:disable()
+	hs.eventtap.keyStroke({}, 'escape')
+	esc_bind:enable()
+end
+
+esc_bind = hs.hotkey.new({}, 'escape', back_to_eng):enable()
